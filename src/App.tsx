312e6fb93d90c43a226f2449616ca63cd4cb38f6
Felipe Parra <fpr29.10@gmail.com>
2024-12-05 13:01:23 -0600

    ✨ feat(App): add CreatedBy component and refactor structure
    - add CreatedBy component to App layout
    - wrap App component in a main tag with full screen dimensions
    - remove semicolons for consistent style throughout file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Account from "./pages/Account";
-import VoiceNoteDetail from "./pages/VoiceNoteDetail";
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as Sonner } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Index from './pages/Index'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Account from './pages/Account'
+import VoiceNoteDetail from './pages/VoiceNoteDetail'
+import CreatedBy from './components/CreatedBy'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Index />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/voice-note/:id" element={<VoiceNoteDetail />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+  <main className="w-screen h-screen relative">
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Index />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/voice-note/:id" element={<VoiceNoteDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+    <CreatedBy />
+  </main>
+)
 
-export default App;
+export default App
