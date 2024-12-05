86ff0005a833c046e4f97a4c99f8cd65b3273e7e
Felipe Parra <fpr29.10@gmail.com>
2024-12-05 13:01:41 -0600

    ✨ feat(components): add CreatedBy component
    - introduce CreatedBy component to display author information
    - include mailto link for bug reporting with accessible styles

---
+++ src/components/CreatedBy.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function CreatedBy() {
+  return (
+    <section className="absolute bottom-0 w-full flex items-center justify-between text-xs text-muted p-4">
+      <article>Created by Felipe Parra with ❤</article>
+      <article className="text-xs text-gray-500">
+        <a
+          href="mailto:hola@felipeparra.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          Report a bug
+        </a>
+      </article>
+    </section>
+  )
+}
